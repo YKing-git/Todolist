86f59a4b6867a189839a09d8bec0430474e04641
YKing-git <kyrz20252001@gmail.com>
2026-03-01 14:09:31 +0900

    PWA safari initial-scale

--- routes/todo.js
+++ routes/todo.js
@@ -127,6 +127,7 @@ router.get("/", async (req, res) => {
   <!-- iOS PWA 対応 -->
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="default">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180.png">
 </head>
 
