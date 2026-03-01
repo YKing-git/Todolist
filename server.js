6fb316ec28b3c7ccf10139de9aabc38403243114
YKing-git <kyrz20252001@gmail.com>
2026-03-01 12:46:25 +0900

    fix express not defined

--- server.js
+++ server.js
@@ -2,13 +2,19 @@
 
 const app = require("./app");
 
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const app = express();
+
+app.use(express.static("public"));
+
 //外部公開版
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.use(express.static("public"));
 
 /*ローカル利用版
 app.listen(3000, "0.0.0.0", () => {         //3000はポート番号（イメージ：PCの3000番の部屋にアクセス）　Node.jsの開発では3000、3001、8080がよく使われる。3000という数字に特に意味はない。
